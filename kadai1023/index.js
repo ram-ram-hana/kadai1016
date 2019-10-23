d4044a9b405f21ed1582f6f104c9cdc3f1ce23de
ram-ram-hana <ram.ram.hana@gmail.com>
2019-10-23 05:17:42 +0000

    Some fix

---
+++ kadai1023/index.js
@@ -0,0 +1,14 @@
+const firstName = 'Kaori'
+const secondName = 'Wato'
+console.log(firstName + secondName)
+
+const sum = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10
+console.log(sum)
+
+const hasCat = false
+const hasDog = true
+
+console.log(hasDog || hasCat)
+console.log(hasDog && hasCat)
+//　犬も猫も飼っていない
+console.log(!hasDog && !hasCat)
